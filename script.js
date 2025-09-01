16977c677cda5a1fe312d5b607e8945a86f4e14c
Shahed Akram Musanna <97869727+musanna121@users.noreply.github.com>
2025-09-01 13:24:44 +0600

    commit

---
+++ script.js
@@ -0,0 +1,154 @@
+// National Emergency Number 
+document.getElementById("copy-national").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const copyCount = parseInt(document.getElementById("total-copy").innerText)
+    newCopyCount = copyCount + 1
+    navigator.clipboard.writeText(999);
+    document.getElementById("total-copy").innerText = newCopyCount
+})
+
+// Police 
+document.getElementById("police-copy").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const copyCount = parseInt(document.getElementById("total-copy").innerText)
+    newCopyCount = copyCount + 1
+    navigator.clipboard.writeText(999);
+    document.getElementById("total-copy").innerText = newCopyCount
+})
+
+// Fire 
+document.getElementById("fire-copy").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const copyCount = parseInt(document.getElementById("total-copy").innerText)
+    newCopyCount = copyCount + 1
+    navigator.clipboard.writeText(999);
+    document.getElementById("total-copy").innerText = newCopyCount
+})
+
+// Health
+document.getElementById("health-copy").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const copyCount = parseInt(document.getElementById("total-copy").innerText)
+    newCopyCount = copyCount + 1
+    navigator.clipboard.writeText("1994-999999");
+    document.getElementById("total-copy").innerText = newCopyCount
+})
+
+// Child 
+document.getElementById("child-copy").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const copyCount = parseInt(document.getElementById("total-copy").innerText)
+    newCopyCount = copyCount + 1
+    navigator.clipboard.writeText(109);
+    document.getElementById("total-copy").innerText = newCopyCount
+})
+
+// Anti-Corruption 
+document.getElementById("corruption-copy").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const copyCount = parseInt(document.getElementById("total-copy").innerText)
+    newCopyCount = copyCount + 1
+    navigator.clipboard.writeText(109);
+    document.getElementById("total-copy").innerText = newCopyCount
+})
+
+// Electricity 
+document.getElementById("electricity-copy").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const copyCount = parseInt(document.getElementById("total-copy").innerText)
+    newCopyCount = copyCount + 1
+    navigator.clipboard.writeText(16216);
+    document.getElementById("total-copy").innerText = newCopyCount
+})
+
+// Brac 
+document.getElementById("brac-copy").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const copyCount = parseInt(document.getElementById("total-copy").innerText)
+    const newCopyCount = copyCount + 1
+    navigator.clipboard.writeText(16445);
+    document.getElementById("total-copy").innerText = newCopyCount
+})
+
+// Railway 
+document.getElementById("rail-copy").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const copyCount = parseInt(document.getElementById("total-copy").innerText)
+    const newCopyCount = copyCount + 1
+    navigator.clipboard.writeText(163);
+    document.getElementById("total-copy").innerText = newCopyCount
+})
+
+// Heart button 
+document.getElementById("like").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const likeCount = parseInt(document.getElementById("total-like").innerText)
+    const newLikeCount = likeCount + 1
+    document.getElementById("total-like").innerText = newLikeCount
+})
+document.getElementById("like-police").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const likeCount = parseInt(document.getElementById("total-like").innerText)
+    const newLikeCount = likeCount + 1
+    document.getElementById("total-like").innerText = newLikeCount
+})
+document.getElementById("like-fire").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const likeCount = parseInt(document.getElementById("total-like").innerText)
+    const newLikeCount = likeCount + 1
+    document.getElementById("total-like").innerText = newLikeCount
+})
+document.getElementById("like-ambulance").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const likeCount = parseInt(document.getElementById("total-like").innerText)
+    const newLikeCount = likeCount + 1
+    document.getElementById("total-like").innerText = newLikeCount
+})
+document.getElementById("like-child").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const likeCount = parseInt(document.getElementById("total-like").innerText)
+    const newLikeCount = likeCount + 1
+    document.getElementById("total-like").innerText = newLikeCount
+})
+document.getElementById("like-anti").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const likeCount = parseInt(document.getElementById("total-like").innerText)
+    const newLikeCount = likeCount + 1
+    document.getElementById("total-like").innerText = newLikeCount
+})
+document.getElementById("like-electricity").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const likeCount = parseInt(document.getElementById("total-like").innerText)
+    const newLikeCount = likeCount + 1
+    document.getElementById("total-like").innerText = newLikeCount
+})
+document.getElementById("like-brac").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const likeCount = parseInt(document.getElementById("total-like").innerText)
+    const newLikeCount = likeCount + 1
+    document.getElementById("total-like").innerText = newLikeCount
+})
+document.getElementById("like-railway").
+addEventListener("click", function(e){
+    e.preventDefault()
+    const likeCount = parseInt(document.getElementById("total-like").innerText)
+    const newLikeCount = likeCount + 1
+    document.getElementById("total-like").innerText = newLikeCount
+})
